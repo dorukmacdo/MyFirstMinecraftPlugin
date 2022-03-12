d70d40d1568206811704951d20fecfedb435515c
dorukmacdo <dorukozdemir104@gmail.com>
2022-03-12 14:16:10 +0300

    2

--- com/macdoplugin/MyFirstPlugin.java
+++ com/macdoplugin/MyFirstPlugin.java
@@ -1,11 +1,14 @@
 package com.macdoplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyFirstPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("Plugin is enabled!");
+
+        Bukkit.getPluginManager().registerEvents(new HelloWorld(), this);
     }
 
     @Override
